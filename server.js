39020a032bfe9280364d8ad5ae33d4d46970a05e
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-16 13:57:51 -0400

    winston logging

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const app = express();
+const cors = require("cors");
+const winston = require("winston");
 
 app.use(bodyParser.json());
+app.use(morgan("tiny"));
+app.use(cors());
 
 app.get("/", (req, res) => {
   return res.send("Hello world");
@@ -11,11 +15,12 @@ app.get("/", (req, res) => {
 
 app.post("/secret", (req, res) => {
   const { userInput } = req.body;
-  console.log(userInput);
-
+  
   if (userInput) {
+    winston.log('info', userInput);
     res.status(200).json("success");
   } else {
+    winston.error("this guy is messing with us");
     res.status(400).json("incorrect submission");
   }
 });
